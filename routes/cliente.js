db21284eed5e79d67bf56630b6f12a07a3f84bdb
hugo valencia jr <huvalpajr10@gmail.com>
2024-06-16 13:46:46 -0500

    Cambio en rutas de metodos de Put a Patch, en ambos modulos de clientes y productos

--- routes/cliente.js
+++ routes/cliente.js
@@ -7,8 +7,8 @@ const clienteController = require('../controllers/clienteController');
 router.post('/', clienteController.agregarClientes);
 router.get('/', clienteController.mostrarClientes);
 router.get('/:id', clienteController.buscarCliente);
-router.put('/:id', clienteController.actualizarClientes);      //Es similar al Patch
-//router.patch('/:id', clienteController.modificarClientes);    //Es similar al Put
+//router.put('/:id', clienteController.actualizarClientes);      //Es similar al Patch
+router.patch('/:id', clienteController.modificarClientes);    //Es similar al Put
 router.delete('/:id', clienteController.eliminarClientes);
 
 
--- routes/producto.js
+++ routes/producto.js
@@ -7,8 +7,8 @@ const productoController = require('../controllers/productoController');
 router.post('/', productoController.agregarProductos);
 router.get('/', productoController.mostrarProductos);
 router.get('/:id', productoController.buscarProducto);
-router.put('/:id', productoController.actualizarProductos);    //Es similar al Patch
-//router.patch('/:id', productoController.modificarProductos); //Es similar al Put
+//router.put('/:id', productoController.actualizarProductos);    //Es similar al Patch
+router.patch('/:id', productoController.modificarProductos); //Es similar al Put
 router.delete('/:id', productoController.eliminarProductos);
 
 
